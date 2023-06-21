b43aca45384311d543e6d07817508bbed4efe812
fch-yk <yefim_korshever@mail.ru>
2023-06-21 14:48:09 +0300

    Add the `Report` model

--- tg_bot/admin.py
+++ tg_bot/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Report
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        'id',
+    ]
+
+    list_display = [
+        'speaker',
+        'topic',
+        'starts_at',
+        'ends_at',
+        'is_current',
+    ]
+
+    ordering = ['starts_at']
--- tg_bot/models.py
+++ tg_bot/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    speaker = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='докладчик',
+        on_delete=models.PROTECT,
+        related_name='reports',
+    )
+    topic = models.CharField(
+        verbose_name='тема',
+        max_length=100
+    )
+
+    starts_at = models.DateTimeField(
+        verbose_name='начало',
+        db_index=True,
+    )
+
+    ends_at = models.DateTimeField(
+        verbose_name='окончание',
+    )
+
+    is_current = models.BooleanField(
+        verbose_name='текущий',
+    )
+
+    class Meta:
+        verbose_name = 'доклад'
+        verbose_name_plural = 'доклады'
