8ad6b4f6e1de95307c101cef1cd110af340ee095
kaser137 <murilon375@gmail.com>
2023-06-22 07:44:45 +0300

    add anketu

--- funcs.py
+++ funcs.py
@@ -1,5 +1,6 @@
 from users.models import User
-from tg_bot.models import Report
+from tg_bot.models import Report, Survey
+from django.db.utils import IntegrityError
 
 
 def identify_user(tg_id):
@@ -27,3 +28,20 @@ def get_report(report_id):
     serialized_report = dict(id=report.id, topic=report.topic, speaker=report.speaker, starts_at=report.starts_at,
                              ends_at=report.ends_at, is_current=report.is_current)
     return serialized_report
+
+
+def create_user(username, tg_1d):
+    try:
+        User.objects.create_user(username=username)
+    except IntegrityError:
+        pass
+    user = User.objects.get(username=username)
+    user.tg_id = tg_1d
+    user.save()
+
+
+def create_survey(username, **data):
+    user = User.objects.get(username=username)
+    Survey.objects.create(user=user, birth_date=data['birth_date'], specialization=data['specialization'],
+                          stack=data['stack'], hobby=data['hobby'], acquaintance_goal=data['acquaintance_goal'],
+                          region=data['region'])
--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import os
 import django
+from datetime import date
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_meetup.settings")
 django.setup()
@@ -23,13 +24,22 @@ class UserState(StatesGroup):
     report = State()
 
 
-@dp.message_handler(text='exit', state=[UserState, None])
+class SurveyState(StatesGroup):
+    birthdate = State()
+    spec = State()
+    stack = State()
+    hobby = State()
+    goal = State()
+    region = State()
+
+
+@dp.message_handler(text='exit', state=[UserState, SurveyState, None])
 async def exit_proceed(msg: types.Message, state: FSMContext):
     await msg.answer('Всего доброго, берегите себя')
     await state.finish()
 
 
-@dp.message_handler(state=[UserState, None])
+@dp.message_handler(state=[not SurveyState, UserState, None])
 async def start_conversation(msg: types.Message):
     status = await sync_to_async(funcs.identify_user)(msg.from_user.id)
     if status == 'hearer':
@@ -44,7 +54,7 @@ async def start_conversation(msg: types.Message):
         await msg.answer('Сделайте выбор', reply_markup=m.reporter_main_menu)
 
 
-@dp.callback_query_handler(Text('reports'), state=[UserState, None])
+@dp.callback_query_handler(Text('reports'), state=[UserState, SurveyState, None])
 async def intro(cb: types.CallbackQuery):
     reports = await sync_to_async(funcs.get_reports)()
     if reports:
@@ -76,11 +86,74 @@ async def manage_report(cb: types.CallbackQuery, state: FSMContext):
 
 
 # hearer block start====================================================================================================
-@dp.callback_query_handler(Text('intro'), state=[UserState, None])
+@dp.callback_query_handler(Text('intro'), state=[UserState, SurveyState, None])
 async def intro(cb: types.CallbackQuery):
     await bot.send_document(chat_id=cb.from_user.id, document=open('intro'),
                             reply_markup=m.hearer_main_menu)
     await cb.answer()
+
+
+@dp.callback_query_handler(Text('form'), state=[UserState, SurveyState, None])
+async def form(cb: types.CallbackQuery):
+    username = cb.from_user.username
+    tg_id = cb.from_user.id
+    await sync_to_async(funcs.create_user)(username, tg_id)
+    await cb.message.answer(f'введите дату рождения в формате "гггг-мм-дд"')
+    await SurveyState.birthdate.set()
+    await cb.answer()
+
+
+@dp.message_handler(state=SurveyState.birthdate)
+async def birthdate(msg: types.Message, state: FSMContext):
+    birth = date.fromisoformat(msg.text)
+    await state.update_data(birth_date=birth)
+    await msg.answer('введите Вашу специализацию')
+    await SurveyState.spec.set()
+
+
+@dp.message_handler(state=SurveyState.spec)
+async def specialization(msg: types.Message, state: FSMContext):
+    specialization = msg.text
+    await state.update_data(specialization=specialization)
+    await msg.answer('введите Ваш стэк')
+    await SurveyState.stack.set()
+
+
+@dp.message_handler(state=SurveyState.stack)
+async def stack(msg: types.Message, state: FSMContext):
+    stack = msg.text
+    await state.update_data(stack=stack)
+    await msg.answer('введите Ваше хобби')
+    await SurveyState.hobby.set()
+
+
+@dp.message_handler(state=SurveyState.hobby)
+async def hobby(msg: types.Message, state: FSMContext):
+    hobby = msg.text
+    await state.update_data(hobby=hobby)
+    await msg.answer('введите цель знакомства')
+    await SurveyState.goal.set()
+
+
+@dp.message_handler(state=SurveyState.goal)
+async def goal(msg: types.Message, state: FSMContext):
+    acquaintance_goal = msg.text
+    await state.update_data(acquaintance_goal=acquaintance_goal)
+    await msg.answer('введите Ваш регион')
+    await SurveyState.region.set()
+
+
+@dp.message_handler(state=SurveyState.region)
+async def proceed_data_for_survey(msg: types.Message, state: FSMContext):
+    region = msg.text
+    username = msg.from_user.username
+    await state.update_data(region=region)
+    survey_data = await state.get_data()
+    print(survey_data)
+    print(username)
+    await sync_to_async(funcs.create_survey)(username, **survey_data)
+    await msg.answer('Good')
+    await state.finish()
 
 
 # hearer block start====================================================================================================
