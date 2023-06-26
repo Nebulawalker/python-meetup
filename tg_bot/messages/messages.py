d7031d4445b32a9bf4561c2e67c4f735079b172b
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-06-26 18:39:34 +0300

    Fix time on report button

--- tg_bot/messages/messages.py
+++ tg_bot/messages/messages.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 ABOUT_MSG = 'Тут будет информация о нас. tg_bot/messages.py var ABOUT_MSG'
 START_SURVEY_MSG = f'Далее Вам необходимо заполнить анкету.\nОбратите внимание на следующие моменты:\n'\
                    f'- Она будет доступна для просмотра другими участниками мероприятия\n'\
@@ -10,10 +12,10 @@ async def get_report_button_caption(report):
     if report['is_current']:
         return f'{report["topic"]}, идет прямо сейчас!'
     else:
-        return f'{report["topic"]}, начало в {report["starts_at"].strftime("%H-%M (%m.%d.%Y)")}'
+        return f'{report["topic"]}, начало в {timezone.localtime(report["starts_at"]).strftime("%H-%M (%m.%d.%Y)")}'
 
 
 async def get_report_description(report):
     description = f'Тема: {report["topic"]},\nСпикер: {report["speaker"]},\n'\
-                  f'Начало в {report["starts_at"].strftime("%H-%M (%m.%d.%Y)")},\nОкончание в {report["ends_at"].strftime("%H-%M (%m.%d.%Y)")}'
+                  f'Начало в {timezone.localtime(report["starts_at"]).strftime("%H-%M (%m.%d.%Y)")},\nОкончание в {timezone.localtime(report["ends_at"]).strftime("%H-%M (%m.%d.%Y)")}'
     return description
